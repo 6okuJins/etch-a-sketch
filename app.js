d20870e1768ff3a313b83f34c8559ddae6666b86
6okuJins <d.ma@mail.utoronto.ca>
2022-04-11 15:32:54 -0400

    Give size slider and clear button functionality

--- app.js
+++ app.js
@@ -4,17 +4,35 @@ let currentSize = 16;
 const body = document.querySelector('body');
 const gridContainer = document.querySelector('.grid-container');
 let drag = false;
-// create 16 * 16 boxes
-for (let i = 0; i < currentSize**2 ; i++) {
-    const gridElement = document.createElement('div');
-    gridContainer.appendChild(gridElement);
-    gridElement.classList.add('grid-element');
-    gridElement.addEventListener('mouseover', draw);
-    gridElement.addEventListener('mousedown', draw);
-    gridElement.addEventListener("mousedown", () => drag = true);
-    gridElement.addEventListener("mouseup", () => drag = false);
+document.addEventListener("mousedown", () => drag = true);
+document.addEventListener("mouseup", () => drag = false);
+
+// create currentSize^2 number of boxes
+function gridSetup() {
+    for (let i = 0; i < currentSize**2 ; i++) {
+        const gridElement = document.createElement('div');
+        gridContainer.appendChild(gridElement);
+        gridElement.classList.add('grid-element');
+        gridElement.addEventListener('mouseover', draw);
+        gridElement.addEventListener('mousedown', draw);
+        gridContainer.style.gridTemplateColumns = `repeat(${currentSize}, minmax(5px, 256px))`;
+        gridContainer.style.gridTemplateRows = `repeat(${currentSize}, minmax(5px, 256px))`;
+    }
 }
 
+function gridClear() {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+}
+let rangeSlider = document.querySelector('#size');
+rangeSlider.addEventListener('click', () => {
+    if (rangeSlider.valueAsNumber != currentSize) {
+        currentSize = rangeSlider.valueAsNumber;
+        gridClear();
+        gridSetup();
+    }
+});
 
 
 
@@ -23,4 +41,8 @@ function draw(e) {
     if (e.type ==='mouseover' && !drag) {return};
     this.style.backgroundColor = currentColor;
     
+}
+
+window.onload = () => {
+    gridSetup();
 }
